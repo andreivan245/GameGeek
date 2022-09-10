ca4b9e5a60b2dbd1b4695460f72e87efead112c9
André Castro <andreflus887@gmail.com>
2022-09-09 21:00:47 -0300

    css

--- assets/js/retornaImg.js
+++ assets/js/retornaImg.js
@@ -1,9 +1,6 @@
 function recarregaPagina() {
-    
-    const elemento = document.querySelector(".produto")
-    const nsei = `<img src="${localStorage.getItem("imagem")}">`
-    elemento.innerHTML = nsei
-    console.log("Deu")                                                                       
+  const elemento = document.querySelector(".produto");
+  const nsei = `<img src="${localStorage.getItem("imagem")}">`;
+  elemento.innerHTML = nsei;
+  console.log("Deu");
 }
-
-
